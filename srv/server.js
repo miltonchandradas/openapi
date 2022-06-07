094f60cf8db8fa83b2b1528ed263e528d091a29c
Milton Chandradas <milton.chandradas@live.com>
2022-06-07 05:48:32 -0400

    rest endpoints...

--- srv/server.js
+++ srv/server.js
@@ -3,6 +3,10 @@ const cds_swagger = require("cds-swagger-ui-express");
 
 cds.on("bootstrap", (app) => {
    app.use(cds_swagger());
+
+   app.get("/service/openapi/hello", (req, res) => {
+      res.send("Hello world...");
+   });
 });
 
 module.exports = cds.server;
